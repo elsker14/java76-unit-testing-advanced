8a4c1f8c3cf47eabe5b50b942db425b168d2fa5c
Iancu Jianu <iancujianu98@gmail.com>
2025-03-20 18:25:35 +0200

    finished unit tests for AnimalRepository

--- src/test/java/mockito_animal/AnimalRepositoryTest.java
+++ src/test/java/mockito_animal/AnimalRepositoryTest.java
@@ -2,10 +2,15 @@ package mockito_animal;
 
 // TDD / BDD
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 /***
  * TDD = test driven development
@@ -20,21 +25,62 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class AnimalRepositoryTest {
 
+    AnimalRepository mockAnimalRepository;
+    AnimalService mockAnimalService;
+
+    @BeforeEach
+    public void setup() {
+        mockAnimalRepository = Mockito.mock(AnimalRepository.class);
+        mockAnimalService = new AnimalService(mockAnimalRepository);
+    }
+
     @Test
     public void testCreateAnimal() {
         // Given - mock the repository - se creeaza o copie care permite asocierea unui comportament dat la nivel de metoda
-        AnimalRepository mockAnimalRepository = Mockito.mock(AnimalRepository.class);
-        AnimalService mockAnimalService = new AnimalService(mockAnimalRepository);
+        /** setup method **/
 
         // When - stubbing behaviour
         Animal expectedResult = new Animal(1, "cat", "Tom");
-        Mockito.when(mockAnimalRepository.save(expectedResult))
+        when(mockAnimalRepository.save(expectedResult))
                 .thenReturn(expectedResult);
         Animal actualResult = mockAnimalRepository.save(new Animal(1, "cat", "Tom"));
 
         // Then - verify the result
         assertThat(actualResult)
                 .isNotNull()
+                .isEqualTo(expectedResult);
+    }
+
+    @Test
+    public void testGetAnimalByIdAndType() {
+        Animal expectedResult = new Animal(2, "dog", "Jerry");
+        when(mockAnimalRepository.findByIdAndType(2, "dog"))
+                .thenReturn(expectedResult);
+
+        Animal actualResult = mockAnimalRepository.findByIdAndType(2, "dog");
+
+        assertThat(actualResult)
+                .isNotNull()
+                .isEqualTo(expectedResult);
+    }
+
+    @Test
+    public void testGetAllAnimals() {
+        List<Animal> expectedResult = new ArrayList<>(
+                List.of(
+                        new Animal(1, "cat", "Tom"),
+                        new Animal(2, "dog", "Jerry"),
+                        new Animal(3, "parrot", "Chicharito")
+                )
+        );
+        when(mockAnimalRepository.findAll()).thenReturn(expectedResult);
+
+        List<Animal> actualResult = mockAnimalRepository.findAll();
+        assertThat(actualResult)
+                .isNotNull()
+                .isNotEmpty()
+                .hasSameSizeAs(expectedResult)
+                .hasSameElementsAs(expectedResult)
                 .isEqualTo(expectedResult);
     }
 }
